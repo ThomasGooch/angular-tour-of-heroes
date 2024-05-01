37f88ebb8f5b6cc57c0588df0eaad788b4393fb7
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-05-01 16:45:18 -0400

    created a detail and service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {id: 1, name: 'Windstorm'};
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[] = [];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+  // hero: Hero = {id: 1, name: 'Windstorm'};
 }
